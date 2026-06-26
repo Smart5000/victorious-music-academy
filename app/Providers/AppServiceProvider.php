d863810f134655270f0442133ff8a4315628cee8
Smart Victorious <iamsmart5000@gmail.com>
2026-06-26 10:49:50 +0100

    Force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\URL;
 use Filament\Tables\Table;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -21,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+	if (app()->environment('production')) {
+    URL::forceScheme('https');
+}
+
         Schema::defaultStringLength(191);
 
         Table::configureUsing(function (Table $table): void {
